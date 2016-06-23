039cf1a40ffce52a60a5c5282c5e59e94b0462e0
Jesse <lkchan0719@gmail.com>
2016-06-23 11:17:21 +0800

    finish most part

--- WordList.py
+++ WordList.py
@@ -5,9 +5,11 @@ import os
 import sys 
 import json
 import urllib2
+import argparse
 from pprint  import pprint
 from colorama import init
 from colorama import Fore, Back, Style
+from collections import deque
 
 init()
 filepath = "./word.md"
@@ -38,12 +40,12 @@ def GetDefinitonFromShanBay(word):
 		print(Fore.RED + "Cannot get the definition\nPlease check out your word:"+Fore.GREEN+"%s") %(word)
 		print(Style.RESET_ALL)
 		sys.exit(0)
-	s    = resp["data"]["definition"].encode('utf-8').strip('\n')
-	definition = " ".join(s.split())
-	return definition
+	return resp
 
 def WriteWordIntoFile(word):
-	definition = GetDefinitonFromShanBay(word)
+	resp       = GetDefinitonFromShanBay(word)
+	s          = resp["data"]["definition"].encode('utf-8').strip('\n')
+	definition = " ".join(s.split())
 	print >> f, "%-20s\t%s" %(word, definition)
 	print(Style.BRIGHT)
 	print(Fore.GREEN + "Add Word Sussess!")
@@ -64,10 +66,9 @@ def CheckFileIsEmpty():
 	else:
 		return 0
 
-def InsertWord():
-	CheckCommandLine()
+def InsertWord(word):
 	# get word
-	word = sys.argv[1].encode('utf-8').strip()
+	#word = sys.argv[1].encode('utf-8').strip()
 	# check file
 	ret = CheckFileIsEmpty()
 	#if the file is not empty, so we need to 
@@ -79,9 +80,87 @@ def InsertWord():
 			sys.exit(0)
 	WriteWordIntoFile(word)
 
+def	ParseCommandLinePara():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('-s',action='store',dest='s_word',
+			help='search a word and get the definition')
+
+	parser.add_argument('-a',action='store',dest='a_word',
+			help='append a word into the word list')
+
+	parser.add_argument('-p',action='store_true',default=False,
+			dest='print_lately',
+			help='print 10 lately words')
+
+	parser.add_argument('-e',action='store_true',default=False,
+			dest='export_excel',
+			help='export word list into excel file')
+	
+	parser.add_argument('-i',action='store_true',default=False,
+			dest="InsertWord",
+			help="Insert word into list when use -s")
+
+	results = parser.parse_args()
+	return results
+
+def GetExampleFromShanBay(w_id):
+	ExampleUrl = 'https://api.shanbay.com/bdc/example/?vocabulary_id='+str(w_id)+'&type=sys'
+	print ExampleUrl
+	u    = urllib2.urlopen(ExampleUrl)
+	resp = json.loads(u.read().decode('utf-8'))
+	if resp.get('data',0) == {}:  # resp is empty
+		return (None,None,0)
+	example   = resp["data"][0]["annotation"]
+	translate = resp["data"][0]["translation"]
+	return (example,translate,1)
+
+def ShowWordDef(word):
+	resp          = GetDefinitonFromShanBay(word)
+	s             = resp["data"]["definition"].encode('utf-8').strip('\n')
+	definition_cn = " ".join(s.split())
+	s             = resp["data"]["en_definition"]["defn"].encode('utf-8').strip('\n')
+	definition_en = " ".join(s.split())
+	word_id       = resp["data"]["id"]
+	#example,translate,Ishave= GetExampleFromShanBay(word_id)
+	print(Style.BRIGHT)
+	print(Fore.RED + word + '\n')
+	print(Fore.GREEN + "[En Def]:\n" +"  " + definition_en)
+	print(Fore.GREEN + "[Cn Def]:\n" +"  " + definition_cn)
+	#if Ishave == 1:
+	#	print(Fore.YELLOW + "Example:\n" + "\t- " + Fore.Green + example)
+	#	print("\t- " + translate)
+	print(Style.RESET_ALL)
+
+
+def ShowLately():
+	while True:
+		print(Style.BRIGHT)
+		for lines in list(deque(f,10)):
+			line_list = lines.split('\t')
+			print(Fore.MAGENTA +  "%-20s" + '\t' + 
+					Fore.CYAN + line_list[1].strip('\n')) %(line_list[0])
+		break
+	print(Style.RESET_ALL)
+	
+
+def ExportList():
+	pass
+
 
 def main():
-	InsertWord()
+	#CheckCommandLine()
+	res = ParseCommandLinePara()
+	if res.a_word:
+		InsertWord(res.a_word)
+	elif res.s_word:
+		ShowWordDef(res.s_word)
+		if res.InsertWord:
+			InsertWord(res.s_word)
+	elif res.print_lately:
+		ShowLately()
+	elif res.export_excel:
+		ExportList()
 
 if __name__ == '__main__':
 	sys.exit(main())
--- word.md
+++ word.md
@@ -4,3 +4,7 @@ good                	n. 善行,好处 adj. 好的,优良的,上等的 [pl.]商�
 generation          	n. 代, 一代
 international       	adj. 国际的 n. 国际组织
 news                	n. 新闻, 消息
+jack                	n. 插座,千斤顶,男人 v. 抬起,提醒,扛举 [Jack] n. 杰克(男子名,John,Jacob的昵称)
+come                	vt. 接近，扮演 vi. 来，发生，达到，进入
+box                 	n. 盒子,箱,专席,亭子,窘境 vt. 装...入盒中,限制住,调和,改变形状,用拳击... vi.参与拳击
+shell               	n. 贝壳,壳,外形 vt. &vi. 去壳,脱落,炮击,拾贝壳 n.[计算机] DOS命令 : 安装备用的COMMAND.COM文件, 并改变环境尺寸
